0e749212063b1ab86f91a27ab219cfb14fddad66
Georg Marth <marth.georg@gmail.com>
2019-06-06 12:11:29 +0200

    init player task

---
+++ Assets/Scripts/TaskLocation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskLocation : MonoBehaviour
+{
+    public PlayerTask Task;
+    public GameObject[] Patrons;
+    public Transform[] Locations;
+}
