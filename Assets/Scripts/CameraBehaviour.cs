23913a6e13f46dcb694f213fbb3f1fb99fddfff4
Georg Marth <marth.georg@gmail.com>
2019-06-12 14:02:05 +0200

    add Player Controller and Placeholder Task Code

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public List<GameObject> Tracked;
+
+    public float MinDistance = 10f;
+    public float DistanceMultiplier = 1f;
+    public float SmoothTime = 1f;
+
+    private Vector3 _currentVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentVelocity = new Vector3();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector2 minMaxHorizontal, minMaxVertical;
+        minMaxHorizontal = minMaxVertical = new Vector2(float.MaxValue, float.MinValue);
+        Vector3 sumPosition = new Vector3();
+
+        for (int i = 0; i < Tracked.Count; i++)
+        {
+            Vector3 objectPosition = Tracked[i].transform.position;
+            sumPosition += objectPosition;
+
+            if(objectPosition.x < minMaxHorizontal.x)
+                minMaxHorizontal.x = objectPosition.x;
+            if(objectPosition.x > minMaxHorizontal.y)
+                minMaxHorizontal.y = objectPosition.x;
+
+            if (objectPosition.z < minMaxVertical.x)
+                minMaxVertical.x = objectPosition.z;
+            if (objectPosition.z > minMaxVertical.y)
+                minMaxVertical.y = objectPosition.z;
+        }
+
+        Vector3 meanPosition = sumPosition / Tracked.Count;
+
+        Vector2 dimensions = new Vector2(minMaxHorizontal.y - minMaxHorizontal.x, minMaxVertical.y - minMaxVertical.x);
+
+        float addDistance = Mathf.Max(dimensions.x, dimensions.y) * DistanceMultiplier;
+        float distance = MinDistance + addDistance;
+
+        Vector3 forward = transform.forward;
+        Vector3 newPosition = meanPosition - (forward * distance);
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref _currentVelocity, SmoothTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerState { IDLE, INTERACTING, HOLDING }
+
+    public float MovementSpeed = 5f;
+    public float RotationSpeed = 0.9f;
+    public float RotationDeadZone = .05f;
+
+    public PlayerState State;
+
+    private Rigidbody rb;
+    private Vector3 MovementInput;
+
+    private TaskLocation currentLocation;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        State = PlayerState.IDLE;
+    }
+
+    void Update()
+    {
+        // INPUT
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        MovementInput = Vector3.ClampMagnitude(new Vector3(horizontal, 0f, vertical), 1f);
+
+        if (Input.GetButtonDown("Fire1") && (State == PlayerState.IDLE || State == PlayerState.HOLDING ) && currentLocation != null)
+        {
+            State = PlayerState.INTERACTING;
+            PerformTask();
+        }
+    }
+
+    private void PerformTask()
+    {
+        StartCoroutine(TestPerform());
+    }
+
+    private IEnumerator TestPerform()
+    {
+        rb.isKinematic = true;
+        yield return new WaitForSeconds(1f);
+        State = PlayerState.IDLE;
+        rb.isKinematic = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TaskLocation location = other.GetComponent<TaskLocation>();
+        if (location == null)
+        {
+            return;
+        }
+        currentLocation = location;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        TaskLocation location = other.GetComponent<TaskLocation>();
+        if (location == null)
+        {
+            return;
+        }
+        if (currentLocation == location)
+        {
+            currentLocation = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (State == PlayerState.IDLE)
+        {
+            // VELOCITY
+            Vector3 movement = MovementInput * MovementSpeed;
+            float vertical = rb.velocity.y;
+            rb.velocity = movement;
+            rb.velocity += new Vector3(0f, vertical, 0f);
+
+            // ROTATION
+            if (MovementInput.sqrMagnitude >= RotationDeadZone)
+            {
+                Vector3 lookDirection = MovementInput.normalized;
+
+                rb.MoveRotation(Quaternion.Slerp(rb.rotation, Quaternion.LookRotation(lookDirection, Vector3.up), RotationSpeed));
+            }
+        }
+    }
+}
